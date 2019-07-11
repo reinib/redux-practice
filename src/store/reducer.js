72e353628f4189779e1c916c3e674e2ee97f1a5b
reinib <brent.reininger89@gmail.com>
2019-07-10 17:15:16 -0700

    Refactor Reducer with Switch-Case

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,25 +3,23 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      counter: state.counter + 1
-    };
-  }
-  if (action.type === "DECREMENT") {
-    return {
-      counter: state.counter - 1
-    };
-  }
-  if (action.type === "ADD") {
-    return {
-      counter: state.counter + action.value
-    };
-  }
-  if (action.type === "SUBTRACT") {
-    return {
-      counter: state.counter - action.value
-    };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.value
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.value
+      };
   }
   return state;
 };
