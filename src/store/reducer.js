5bf1ad225439dad3e51d8cb5e609f2f39dfbcba2
reinib <brent.reininger89@gmail.com>
2019-07-11 11:31:08 -0700

    Outsource Action Types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
   counter: 0,
   results: []
@@ -5,34 +7,34 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case actionTypes.INCREMENT:
       // One option for immutably updating state
       const newState = Object.assign({}, state);
       newState.counter = state.counter + 1;
       return newState;
-    case "DECREMENT":
+    case actionTypes.DECREMENT:
       return {
         // ES6 way for immutably updating state, USE THIS WAY
         ...state,
         counter: state.counter - 1
       };
-    case "ADD":
+    case actionTypes.ADD:
       return {
         ...state,
         counter: state.counter + action.value
       };
-    case "SUBTRACT":
+    case actionTypes.SUBTRACT:
       return {
         ...state,
         counter: state.counter - action.value
       };
-    case "STORE_RESULT":
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         // Concat returns new array thus is an immutable way to push items to an array
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
-    case "DELETE_RESULT":
+    case actionTypes.DELETE_RESULT:
       //   less optimal way
       //   const id = 2
       //   const newArray = [...state.results]
